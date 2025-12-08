a77e0f58dffcdaba4c786424aca70984bb5c949a
Vlad <ciorescuvladstefan0@gmail.com>
2025-12-08 15:44:31 +0200

    changed some naming and added some fonts

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,9 +1,11 @@
 import Stripe from "stripe";
 import "../../../envConfig.js";
 
-const API_KEY = process.env.STRIPE_SECRET_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
 
-const stripe = new Stripe(API_KEY);
+const stripe = new Stripe(API_KEY,{
+  apiVersion: '2023-10-16'
+});
 
 //Stripe get request to get the product and filter data
 export async function GET() {
